f4b4f6bc815c9ad2c433b670352bf58fb16182b7
AudeNec <aude.nec@gmail.com>
2024-11-07 16:27:55 +0100

    create game page

---
+++ src/pages/Game.tsx
@@ -0,0 +1,33 @@
+import "./Game.css";
+
+import AlcoholLevel from "../components/AlcoholLevel";
+import GameSet from "../components/GameSet";
+
+function Game() {
+	return (
+		<main id="game-main">
+			<section>
+				<div id="game-alcohol-level">
+					<AlcoholLevel />
+				</div>
+			</section>
+			<section id="game-gameset">
+				<GameSet />
+			</section>
+			<section>
+				<article id="game-rules">
+					<h3>Comment jouer ?</h3>
+					<p>
+						Bienvenue dans le jeu des bières ! <br />
+						Jouez 5 manches contre l’ordinateur. Chaque défaite augmente votre
+						taux d'alcoolémie virtuel.
+						<br />
+						Bon jeu, et santé !
+					</p>
+				</article>
+			</section>
+		</main>
+	);
+}
+
+export default Game;
