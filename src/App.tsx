9dcf81e51a3db177c354b1615c1a9de69ccd3b6c
Julien <juju.gruet@mail.com>
2024-11-07 15:57:35 +0100

    nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
 import Navigation from "./component/Nav";
 
@@ -5,6 +6,7 @@ function App() {
 	return (
 		<>
 			<Navigation />
+			<Outlet />
 		</>
 	);
 }
