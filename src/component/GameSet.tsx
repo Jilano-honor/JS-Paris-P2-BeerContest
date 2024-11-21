d45c3675b83c8bd61d0f61c3780969620d4810f9
Julien <juju.gruet@mail.com>
2024-11-21 22:37:19 +0100

    end of alcohollevel a refaire apres les cartes

--- src/component/GameSet.tsx
+++ src/component/GameSet.tsx
@@ -92,6 +92,22 @@ function GameSet() {
 			setAlcoholLevel((prev) => prev + userAbv);
 		}
 	};
+	useEffect(() => {
+		if (alcoholLevel >= 15) {
+			setDecks(([userDeck, computerDeck]) => [
+				shuffleDeck(userDeck),
+				shuffleDeck(computerDeck),
+			]);
+		}
+	}, [alcoholLevel]);
+	const shuffleDeck = (deck: BeerProps[]) => {
+		const shuffledDeck = [...deck];
+		for (let i = shuffledDeck.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[shuffledDeck[i], shuffledDeck[j]] = [shuffledDeck[j], shuffledDeck[i]];
+		}
+		return shuffledDeck;
+	};
 
 	const handleUserCardSelect = (selectedCard: BeerProps) => {
 		const updatedComputerDeck = decks[1];
