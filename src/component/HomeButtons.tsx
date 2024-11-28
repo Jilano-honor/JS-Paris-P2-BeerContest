03cd9d2e701d44fc679995553392d3043b6a160e
Julien Gruet-demonet <juju.gruet@gmail.com>
2024-11-28 15:04:39 +0100

    az

--- src/component/HomeButtons.tsx
+++ src/component/HomeButtons.tsx
@@ -8,13 +8,13 @@ const HomeButtons = () => {
 			<Link to="/Test">
 				<button type="button" className="home-button">
 					<p id="deck-button-text">Construire mon deck</p>
-					<img src="src/assets/deck.png" alt="logo deck" id="logo-deck" />
+					<img src="../src/assets/deck.png" alt="logo deck" id="logo-deck" />
 				</button>
 			</Link>
 			<Link to="/game">
 				<button type="button" className="home-button">
 					<p>Jouer</p>
-					<img src="src/assets/picto-beer.png" alt="logo bière" />
+					<img src="../src/assets/picto-beer.png" alt="logo bière" />
 				</button>
 			</Link>
 		</div>
